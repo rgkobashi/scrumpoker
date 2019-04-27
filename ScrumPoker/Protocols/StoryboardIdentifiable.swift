139f22c8f0e1c254433e1ea38879859dbe1406de
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 17:45:10 +0900

    adding StoryboardIdentifiable

---
+++ ScrumPoker/Protocols/StoryboardIdentifiable.swift
@@ -0,0 +1,30 @@
+//
+//  StoryboardIdentifiable.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+protocol StoryboardIdentifiable {
+    static var storyboardIdentifier: String { get }
+}
+
+extension StoryboardIdentifiable where Self: UIViewController {
+    static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UIStoryboard {
+    func instantiateViewController<T: UIViewController>() -> T {
+        guard let viewController = self.instantiateViewController(withIdentifier: T.storyboardIdentifier) as? T else {
+            fatalError("Couldn't instantiate view controller with identifier \(T.storyboardIdentifier) ")
+        }
+        return viewController
+    }
+}
+
+extension UIViewController: StoryboardIdentifiable { }
