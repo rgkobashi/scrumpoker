978829caeae8a4a7daba2d1f8be70fe2e445eaf2
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 19:06:15 +0900

    adding card model

---
+++ ScrumPoker/Models/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    let text: String
+}
