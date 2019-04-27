e0686136e1e43b92dca8453137bf94d3a1c73c71
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 17:43:46 +0900

    adding UIStoryboard extension

---
+++ ScrumPoker/Extensions/UIStoryboard.swift
@@ -0,0 +1,15 @@
+//
+//  UIStoryboard.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    convenience init(name: String) {
+        self.init(name: name, bundle: nil)
+    }
+}
