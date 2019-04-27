5b43726f84dfefbebbb09e019ce8c90935fa2118
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 18:21:02 +0900

    adding ReusableView protocol

---
+++ ScrumPoker/Protocols/ReusableView.swift
@@ -0,0 +1,21 @@
+//
+//  ReusableView.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: ReusableView {}
