d8b9eaec1344a28c7ca85ab15bb7a3912bc58526
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 18:00:27 +0900

    adding CardCell xib

---
+++ ScrumPoker/Scenes/Main/Cells/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    
+}
