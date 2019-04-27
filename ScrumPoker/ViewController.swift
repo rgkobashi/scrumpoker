6d68f0874a8c3a124399f84f35d98196df8cb9ba
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-27 16:39:43 +0900

    Initial Commit

---
+++ ScrumPoker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/27.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
