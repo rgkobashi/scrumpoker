4e0a400f42ea62524b0a721ffc0ad91ec5d2fb97
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-29 01:28:52 +0900

    adding SettingsCell

---
+++ ScrumPoker/Scenes/Settings/Cells/SettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsCell.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/29.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
