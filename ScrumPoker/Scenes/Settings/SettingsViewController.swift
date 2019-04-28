86b77e0c15a59222270ef660c2bc1245f43ffd04
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-29 01:02:28 +0900

    adding tableView on settingsVC and binding it with the VM

--- ScrumPoker/Scenes/Settings/SettingsViewController.swift
+++ ScrumPoker/Scenes/Settings/SettingsViewController.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        }
+    }
+    
     var viewModel: SettingsViewModel!
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSections
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows(in: section)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.headerTitle(in: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let rvm = viewModel.rowViewModel(for: indexPath)
+        cell.textLabel?.text = rvm.text
+        return cell
+    }
 }
--- ScrumPoker/Scenes/Settings/SettingsViewModel.swift
+++ ScrumPoker/Scenes/Settings/SettingsViewModel.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
+struct SettingsSectionViewModel {
+    let title: String?
+    let rows: [RowViewModel]
+}
+
+struct SettingsRowViewModel: RowViewModel {
+    let text: String
+    let type: RowType
+}
+
 class SettingsViewModel {
+    
+    private let settings: [SettingsSectionViewModel]
+    
+    init(settings: [SettingsSectionViewModel]) {
+        self.settings = settings
+    }
+    
+    var numberOfSections: Int {
+        return settings.count
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return settings[section].rows.count
+    }
+    
+    func headerTitle(in section: Int) -> String? {
+        return settings[section].title
+    }
+    
+    func rowViewModel(for indexPath: IndexPath) -> RowViewModel {
+        return settings[indexPath.section].rows[indexPath.row]
+    }
 }
