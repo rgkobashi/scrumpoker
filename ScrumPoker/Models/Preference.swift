3c4cc3a0f2afc98b8bb1284def93638e141c1429
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-04-30 02:49:03 +0900

    adding Preference model

---
+++ ScrumPoker/Models/Preference.swift
@@ -0,0 +1,20 @@
+//
+//  Preference.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/04/30.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import Foundation
+
+struct Preference<T> {
+    let name: String
+    let id: String
+}
+
+extension Preference {
+    static var shakeToReveal: Preference<Bool> {
+        return Preference<Bool>(name: "Shake to reveal", id: "shakeToReveal")
+    }
+}
