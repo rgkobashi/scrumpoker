0587bbdf8362a992ae2fed161cecd4ee69e05763
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-07 00:25:13 +0900

    adding ActionRowViewModel model

---
+++ ScrumPoker/Models/ActionRowViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ActionRowViewModel.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/12/07.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import Foundation
+
+struct ActionRowViewModel: TableRowViewModel {
+    let text: String
+    let action: () -> ()
+    
+    var type: TableRowType {
+        return .unspecified
+    }
+    
+    func didSelect() {
+        action()
+    }
+}
