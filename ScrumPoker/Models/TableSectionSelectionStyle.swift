ec6f6cbd555075f4d1795141bdc23094ceb41f19
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-07 00:10:30 +0900

    adding TableSectionSelectionStyle model

---
+++ ScrumPoker/Models/TableSectionSelectionStyle.swift
@@ -0,0 +1,15 @@
+//
+//  TableSectionSelectionStyle.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/12/07.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import Foundation
+
+enum TableSectionSelectionStyle {
+    case single
+    case multiple
+    case none
+}
