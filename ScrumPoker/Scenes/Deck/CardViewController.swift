a953c9ab426f63dc9652300668bd0e91f2cb383e
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-07 22:23:14 +0900

    adding CardViewController on Deck storyboard

---
+++ ScrumPoker/Scenes/Deck/CardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewController.swift
+//  ScrumPoker
+//
+//  Created by Rogelio Kobashi on 2019/12/07.
+//  Copyright © 2019 rgkobashi. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
