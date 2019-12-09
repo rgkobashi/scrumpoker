2112bb8f6043952985198b1e4f613c2932126c64
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-09 23:14:33 +0900

    adding viewDelegate for DeckViewModel to be called when the deck has been updated

--- ScrumPoker/Scenes/Deck/ViewModels/DeckViewModel.swift
+++ ScrumPoker/Scenes/Deck/ViewModels/DeckViewModel.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
+protocol DeckViewModelViewDelegate: class {
+    func didUpdateDeck()
+}
+
 protocol DeckViewModelDelegate: class {
     func didTapShowMenu(from viewController: DeckViewController)
     func didTapShowCard(_ card: Card, from viewController: DeckViewController)
 }
 
 class DeckViewModel {
-    private let deck: Deck
+    private var deck: Deck
     private let layout: DeckLayout
     
     weak var delegate: DeckViewModelDelegate?
+    weak var viewDelegate: DeckViewModelViewDelegate?
     
     init(deck: Deck, layout: DeckLayout) {
         self.deck = deck
         self.layout = layout
+    }
+    
+    func updateDeck(_ deck: Deck) {
+        self.deck = deck
+        viewDelegate?.didUpdateDeck()
     }
     
     // MARK: Data source
