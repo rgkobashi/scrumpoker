240c7b14d5f49de9e1b5f307a2e0db3982c9e4b1
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-14 19:48:33 +0900

    adding docs to DeckLayout and fixing layout

--- ScrumPoker/Models/DeckLayout.swift
+++ ScrumPoker/Models/DeckLayout.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
+/// For a layout that does not require scrolling use the listed formulas:
+/// - (cardWidth * X) + (horizontalCardSpacing * (X-1)) = 1
+/// - (cardHeight * Y) + (verticalCardSpacing * (Y-1)) = 1
+///
+/// Where `X` is the number of cards per row and `Y` number of cards per column.
 struct DeckLayout {
+    /// Percentage of card containerView width to use for card width expressed from 0 to 1.
     let cardWidth: CGFloat
+    /// Percentage of card containerView height to use for card height expressed from 0 to 1.
     let cardHeight: CGFloat
     
+    /// Percentage of card containerView width to use for horizontal card spacing expressed from 0 to 1.
     let horizontalCardSpacing: CGFloat
+    /// Percentage of card containerView height to use for vertical card spacing expressed from 0 to 1.
     let verticalCardSpacing: CGFloat
     
+    /// Percentage of deck containerView width to use for horizontal deck padding expressed from 0 to 1.
     let horizontalDeckPadding: CGFloat
+    /// Percentage of deck containerView height to use for vertical deck padding expressed from 0 to 1.
     let verticalDeckPadding: CGFloat
 }
 
 extension DeckLayout {
-    static let `default` = DeckLayout(cardWidth: 0.20,
-                                      cardHeight: 0.15,
-                                      horizontalCardSpacing: 0.06,
-                                      verticalCardSpacing: 0.04,
-                                      horizontalDeckPadding: 0.27,
-                                      verticalDeckPadding: 0.08)
+    /// Layout for 3 cards per row and 5 per column without the need of scrolling.
+    static let `default` = DeckLayout(cardWidth: 0.25,
+                                      cardHeight: 0.166,
+                                      horizontalCardSpacing: 0.125,
+                                      verticalCardSpacing: 0.0425,
+                                      horizontalDeckPadding: 0.1,
+                                      verticalDeckPadding: 0.1)
 }
