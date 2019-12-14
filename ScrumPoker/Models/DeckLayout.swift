ba127df34fb04b21f61e1aaa895dac5a3bb5d62e
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2019-12-15 02:00:19 +0900

    updating layout

--- ScrumPoker/Models/DeckLayout.swift
+++ ScrumPoker/Models/DeckLayout.swift
@@ -32,7 +32,7 @@ struct DeckLayout {
 
 extension DeckLayout {
     /// Layout for 3 cards per row and 5 per column without the need of scrolling.
-    static let `default` = DeckLayout(cardWidth: 0.28,
+    static let `default` = DeckLayout(cardWidth: 0.275,
                                       cardHeight: 0.166,
                                       horizontalCardSpacing: 0.08,
                                       verticalCardSpacing: 0.0425,
