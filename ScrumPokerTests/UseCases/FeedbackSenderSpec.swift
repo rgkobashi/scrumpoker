acdb98734fdbeb2a7424c2b313b3cb84b7c5e6dd
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 00:14:00 +0900

    adding UTs for FeedbackSenderSpec

--- ScrumPokerTests/UseCases/FeedbackSenderSpec.swift
+++ ScrumPokerTests/UseCases/FeedbackSenderSpec.swift
@@ -7,10 +7,56 @@
 //
 
 @testable import ScrumPoker
+import MessageUI
 import Quick
 import Nimble
 
 class FeedbackSenderSpec: QuickSpec {
     override func spec() {
+        var sut: FeedbackSender!
+        
+        describe("sendFeedback") {
+            context("mail type") {
+                context("if Mail is configured") {
+                    beforeEach {
+                        StubMailComposeVC.canSendMailToReturn = true
+                        sut = FeedbackSender(mailComposeViewControllerType: StubMailComposeVC.self)
+                    }
+                    it("shows mail compose screen") {
+                        let vc = MockVC()
+                        try? sut.sendFeedback(.mail(recipients: [""], subject: "", message: "", from: vc))
+                        expect(vc.presentViewControllerCalled) == true
+                    }
+                }
+                context("if Mail is not configured") {
+                    beforeEach {
+                        StubMailComposeVC.canSendMailToReturn = false
+                        sut = FeedbackSender(mailComposeViewControllerType: StubMailComposeVC.self)
+                    }
+                    it("throws Error.mailClientNotConfigured") {
+                        expect {
+                            try sut.sendFeedback(.mail(recipients: [""], subject: "", message: "", from: UIViewController()))
+                        }.to(throwError(FeedbackSender.Error.mailClientNotConfigured))
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+private class StubMailComposeVC: MFMailComposeViewController {
+    static var canSendMailToReturn: Bool!
+    
+    override class func canSendMail() -> Bool {
+        return canSendMailToReturn
+    }
+}
+
+private class MockVC: UIViewController {
+    var presentViewControllerCalled = false
+    
+    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        presentViewControllerCalled = true
     }
 }
