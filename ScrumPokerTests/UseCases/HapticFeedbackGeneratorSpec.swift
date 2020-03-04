d9da90c9fe4c67c8eea5f521e00b3e7d40bcb3a2
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 00:25:09 +0900

    adding UTs for HapticFeedbackGenerator

--- ScrumPokerTests/UseCases/HapticFeedbackGeneratorSpec.swift
+++ ScrumPokerTests/UseCases/HapticFeedbackGeneratorSpec.swift
@@ -12,6 +12,37 @@ import Nimble
 
 class HapticFeedbackGeneratorSpec: QuickSpec {
     override func spec() {
+        var generator: MockGenerator!
         var sut: HapticFeedbackGenerator!
+        
+        describe("generate") {
+            context("success type") {
+                beforeEach {
+                    generator = MockGenerator()
+                    sut = HapticFeedbackGenerator(generator: generator)
+                }
+                it("calls prepare") {
+                    sut.generate(.success)
+                    expect(generator.isPrepareCalled) == true
+                }
+                it("occurres success type") {
+                    sut.generate(.success)
+                    expect(generator.notificationTypeOccurred) == .success
+                }
+            }
+        }
+    }
+}
+
+private class MockGenerator: UINotificationFeedbackGenerator {
+    var isPrepareCalled = false
+    var notificationTypeOccurred: UINotificationFeedbackGenerator.FeedbackType?
+    
+    override func prepare() {
+        isPrepareCalled = true
+    }
+    
+    override func notificationOccurred(_ notificationType: UINotificationFeedbackGenerator.FeedbackType) {
+        notificationTypeOccurred = notificationType
     }
 }
