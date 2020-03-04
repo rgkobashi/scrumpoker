49d0735629c3a2e37c16c01f2383c1d0b0e0e212
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 00:32:55 +0900

    adding UTs for AnalyticsManager

--- ScrumPokerTests/UseCases/AnalyticsManagerSpec.swift
+++ ScrumPokerTests/UseCases/AnalyticsManagerSpec.swift
@@ -12,6 +12,27 @@ import Nimble
 
 class AnalyticsManagerSpec: QuickSpec {
     override func spec() {
+        var engine: MockEngine!
         var sut: AnalyticsManager!
+        
+        describe("log") {
+            beforeEach {
+                engine = MockEngine()
+                sut = AnalyticsManager(engine: engine)
+            }
+            it("sends right event to the engine") {
+                let event = AnalyticsEvent.shareApp
+                sut.log(event)
+                expect(engine.analyticsEventSent) == event
+            }
+        }
+    }
+}
+
+private class MockEngine: AnalyticsEngine {
+    var analyticsEventSent: AnalyticsEvent!
+    
+    func sendAnalyticsEvent(_ event: AnalyticsEvent) {
+        analyticsEventSent = event
     }
 }
