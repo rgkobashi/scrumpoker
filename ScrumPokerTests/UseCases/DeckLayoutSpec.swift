954abe19e0dff558767a69ef9681a07cf7b915c3
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 01:15:52 +0900

    adding DeckLayoutSpec

---
+++ ScrumPokerTests/UseCases/DeckLayoutSpec.swift
@@ -0,0 +1,17 @@
+//
+//  DeckLayoutSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/05.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class DeckLayoutSpec: QuickSpec {
+    override func spec() {
+        var sut: DeckLayout!
+    }
+}
