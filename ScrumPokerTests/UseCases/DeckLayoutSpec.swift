617a01ab875dfd0deb1103cc7c072d498009c3a7
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 01:34:35 +0900

    adding UTs for DeckLayout

--- ScrumPokerTests/UseCases/DeckLayoutSpec.swift
+++ ScrumPokerTests/UseCases/DeckLayoutSpec.swift
@@ -13,5 +13,24 @@ import Nimble
 class DeckLayoutSpec: QuickSpec {
     override func spec() {
         var sut: DeckLayout!
+        
+        describe("default which is 3x5") {
+            context("with no need of horizontal scrolling") {
+                it("formula result should be close to 1") {
+                    sut = DeckLayout.default
+                    let X: CGFloat = 3
+                    let result = (sut.cardWidth * X) + (sut.horizontalCardSpacing * (X-1))
+                    expect(result).to(beCloseTo(1, within: 0.02))
+                }
+            }
+            context("with no need of vertical scrolling") {
+                it("formula result should be close to 1") {
+                    sut = DeckLayout.default
+                    let Y: CGFloat = 5
+                    let result = (sut.cardHeight * Y) + (sut.verticalCardSpacing * (Y-1))
+                    expect(result).to(beCloseTo(1, within: 0.02))
+                }
+            }
+        }
     }
 }
