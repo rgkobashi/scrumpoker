b10797636d064217f147fdefce6f60795e22b310
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-04 23:51:59 +0900

    adding FeedbackSenderSpec and removing old tests

---
+++ ScrumPokerTests/UseCases/FeedbackSenderSpec.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackSenderSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/04.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class FeedbackSenderSpec: QuickSpec {
+    override func spec() {
+    }
+}
