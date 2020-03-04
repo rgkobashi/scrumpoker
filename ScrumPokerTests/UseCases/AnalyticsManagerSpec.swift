81496e3e98849def170b3abe68ab5a0b3960ba17
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 00:25:47 +0900

    adding AnalyticsManagerSpec

---
+++ ScrumPokerTests/UseCases/AnalyticsManagerSpec.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsManagerSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/05.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class AnalyticsManagerSpec: QuickSpec {
+    override func spec() {
+        var sut: AnalyticsManager!
+    }
+}
