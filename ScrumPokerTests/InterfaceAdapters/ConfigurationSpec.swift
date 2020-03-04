5125ce6438e490f4ca3260909f3560f8da679c4e
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-05 00:34:05 +0900

    adding ConfigurationSpec

---
+++ ScrumPokerTests/InterfaceAdapters/ConfigurationSpec.swift
@@ -0,0 +1,16 @@
+//
+//  ConfigurationSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/05.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class ConfigurationSpec: QuickSpec {
+    override func spec() {
+    }
+}
