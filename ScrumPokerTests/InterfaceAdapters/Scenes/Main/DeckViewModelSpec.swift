6ae988f974daecd0ebbca85178fa33104c70fc80
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-07 00:32:57 +0900

    adding UT for DeckViewModel.updateDeck

--- ScrumPokerTests/InterfaceAdapters/Scenes/Main/DeckViewModelSpec.swift
+++ ScrumPokerTests/InterfaceAdapters/Scenes/Main/DeckViewModelSpec.swift
@@ -12,6 +12,34 @@ import Nimble
 
 class DeckViewModelSpec: QuickSpec {
     override func spec() {
+        var initialDeck: Deck!
+        var layout: DeckLayout!
+        var viewDelegate: ViewDelegate!
         var sut: DeckViewModel!
+        
+        beforeEach {
+            initialDeck = Deck(id: "1", name: "testing", cards: [])
+            layout = DeckLayout(cardWidth: 0, cardHeight: 0, horizontalCardSpacing: 0, verticalCardSpacing: 0, horizontalDeckPadding: 0, verticalDeckPadding: 0)
+            viewDelegate = ViewDelegate()
+        }
+        
+        describe("updateDeck") {
+            it("notifies view") {
+                sut = DeckViewModel(deck: initialDeck, layout: layout)
+                sut.viewDelegate = viewDelegate
+                
+                sut.updateDeck(Deck(id: "2", name: "testing", cards: []))
+                expect(viewDelegate.isDidUpdateDeckCalled) == true
+            }
+        }
+    }
+}
+
+// MARK: - Test doubles
+
+private class ViewDelegate: DeckViewModelViewDelegate {
+    var isDidUpdateDeckCalled = false
+    func didUpdateDeck() {
+        isDidUpdateDeckCalled = true
     }
 }
