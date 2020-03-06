8fbaa15209f5be15634012d340e991da427b2d0f
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-07 01:10:52 +0900

    adding MenuViewModelSpec

---
+++ ScrumPokerTests/InterfaceAdapters/Scenes/Menu/MenuViewModelSpec.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewModelSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/07.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class MenuViewModelSpec: QuickSpec {
+    override func spec() {
+        var sut: MenuViewModel!
+    }
+}
