7c7ef500b54c715f260f92dbbc30b4508a1114e2
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2020-03-07 00:27:16 +0900

    adding DeckViewModelSpec

---
+++ ScrumPokerTests/InterfaceAdapters/Scenes/Main/DeckViewModelSpec.swift
@@ -0,0 +1,17 @@
+//
+//  DeckViewModelSpec.swift
+//  ScrumPokerTests
+//
+//  Created by Rogelio Kobashi on 2020/03/07.
+//  Copyright © 2020 rgkobashi. All rights reserved.
+//
+
+@testable import ScrumPoker
+import Quick
+import Nimble
+
+class DeckViewModelSpec: QuickSpec {
+    override func spec() {
+        var sut: DeckViewModel!
+    }
+}
